7d5b4cce55d21cd6eef054196272a53dacdc89ad
rkennedy1 <kennedy@smu.edu>
2020-04-04 20:05:56 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Backend.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Backend.exe"
+  "Backend.exe.manifest"
+  "Backend.lib"
+  "Backend.pdb"
+  "CMakeFiles/Backend.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ryan Kennedy on 4/4/2020.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
