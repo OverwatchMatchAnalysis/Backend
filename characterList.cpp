b6c097b7545746a50205b29f69f338b6487064e0
wildermason <wildermason20@gmail.com>
2020-04-05 16:28:42 -0500

    updated characterList

--- characterList.cpp
+++ characterList.cpp
@@ -3,4 +3,39 @@
 //
 
 #include "characterList.h"
+using namespace std;
 
+characterList::characterList()
+{
+
+}
+
+void characterList::setName(string name)
+{
+    characterName = name;
+}
+
+string characterList::getName()
+{
+    return characterName;
+}
+
+void characterList::setEnemyValue(double value)
+{
+    enemyValue = value;
+}
+
+double characterList::getEnemyValue()
+{
+    return enemyValue;
+}
+
+void characterList::setFriendlyValue(double value)
+{
+    friendlyValue = value;
+}
+
+double characterList::getFriendlyValue()
+{
+    return friendlyValue;
+}
--- characterList.h
+++ characterList.h
@@ -7,9 +7,18 @@
 class characterList
 {
     public:
+        characterList();
+        void setName(std::string name);
+        std::string getName();
+        void setEnemyValue(double value);
+        double getEnemyValue();
+        void setFriendlyValue(double value);
+        double getFriendlyValue();
 
     private:
-
+        std::string characterName = "TestName";
+        double enemyValue = 0;
+        double friendlyValue = 0;
 };
 
 #endif //CHARACTERLIST_H
