802f23c0717374f6b206c3a00eb4097dabb2d374
wildermason <wildermason20@gmail.com>
2020-04-05 15:30:59 -0500

    Created Files

---
+++ characterList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wilde on 4/5/2020.
+//
+
+#include "characterList.h"
---
+++ characterList.h
@@ -0,0 +1,19 @@
+#ifndef CHARACTERLIST_H
+#define CHARACTERLIST_H
+
+#include <string>
+
+class characterList
+{
+    public:
+
+    private:
+        std::string characterNames[31] = {"Ana", "Ashe", "Baptiste", "Bastion", "Brigitte", "D.Va", "Doomfist", "Genji",
+                                          "Hanzo", "Junkrat", "Lucio", "McCree", "Mei", "Mercy", "Moira", "Orisa",
+                                          "Pharah", "Reaper", "Reinhardt", "Roadhog", "Sigma", "Soldier: 76", "Sombra",
+                                          "Symmetra", "Torbjorn", "Tracer", "Widowmaker", "Winston", "Wrecking Ball",
+                                          "Zarya", "Zenyatta"};
+};
+
+
+#endif //CHARACTERLIST_H
